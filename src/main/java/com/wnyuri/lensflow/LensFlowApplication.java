8f67dfeb515d49e1f218950f75ae06fdd5940f6f
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-01-30 22:58:04 -0300

    Initial commit with dependencies

---
+++ src/main/java/com/wnyuri/lensflow/LensFlowApplication.java
@@ -0,0 +1,13 @@
+package com.wnyuri.lensflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LensFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wnyuri/lensflow/LensFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnyuri.lensflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LensFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
